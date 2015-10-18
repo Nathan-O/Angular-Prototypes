8acbb9d3585494795cdf53681dedc45c2e046c9c
Nathan Ockerman <ockerman.ns@gmail.com>
2015-10-18 15:29:30 -0700

    data rendering on index.

--- Forum/app-forum.js
+++ Forum/app-forum.js
@@ -1,85 +1,223 @@
 console.log("Sanity Forum");
 
-app.directive('ngIf', function() {
-    return {
-        link: function(scope, element, attrs) {
-            if(scope.$eval(attrs.ngIf)) {
-                // remove '<div ng-if...></div>'
-                element.replaceWith(element.children());
-            } else {
-                element.replaceWith(' ');
-            }
-        }
-    };
+var forumApp = angular.module("forumApp", ['ngRoute']);
+
+////////////
+// ROUTES //
+////////////
+
+forumApp.config(function($routeProvider, $locationProvider){
+
+   $routeProvider
+      .when('/', {
+      templateUrl: 'Forum/templates/forum-index.html',
+      controller: 'ForumIndexCtrl'
+   })
+      .when('/forums/:id', {
+      // template: '{{wine.name}}',
+      templateUrl: 'Forum/templates/forum-show.html',
+      controller: 'ForumShowCtrl'
+   })
+      .otherwise({
+      redirectTo: '/'
+   });
+
+   $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+   });
+
+});
+
+/////////////////
+// CONTROLLERS //
+/////////////////
+
+forumApp.controller('ForumIndexCtrl',function($scope, DiscussionData){
+   console.log("Forum Index");
+   $scope.forums = DiscussionData.query();
+});
+
+forumApp.controller('ForumShowCtrl',function($scope, DiscussionData, $routeParams){
+   console.log("Forum Show");
+   $scope.forum = DiscussionData.get($routeParams.id);
+});
+
+////////////
+// MODELS //
+////////////
+
+forumApp.factory('DiscussionData', function(){
+
+   var DiscussionData = {};
+
+   DiscussionData.query = function(){
+      return FORUM_DATA;
+   };
+
+   DiscussionData.get = function(id){
+      var dataId = parseInt(id);
+      return FORUM_DATA.find(function(wine){
+         return wine.id == dataId;
+      });
+   };
+
+  return DiscussionData;
+
 });
 
 
-app.controller("MyCtrl", function($scope) {
-    $scope.data = {};
-    $scope.showFrom = function(message) {
-        return message.hasOwnProperty('from');
-    };
-    $scope.showCreatedBy= function(message) {
-        return message.hasOwnProperty('createdBy');
-    };
-    $scope.showTo= function(message) {
-        return message.hasOwnProperty('to');
-    };
-    $scope.data.messages = [
-        {
-        "type": "phone",
-        "date": "25/12/2012",
-        "time": "11.34",
-        "createdBy": {
-            "name": "Jenny Forster1",
-            "avatarFileName": "jenny-forster.jpg"
-        },
-        "from": {
-            "name": "Jenny Forster1",
-            "avatarFileName": "jenny-forster.jpg"
-        },
-        "to": {
-            "name": "Daniel Craig1",
-            "avatarFileName": "daniel-craig.jpg"
-        },
-        "title": "This is the title of a phone call"},
-    {
-        "type": "email",
-        "date": "25/12/2012",
-        "time": "11.34",
-        "createdBy": {
-            "name": "Jenny Forster2",
-            "avatarFileName": "jenny-forster.jpg"
-        },
-        "from": {
-            "name": "Daniel Craig2",
-            "avatarFileName": "daniel-craig.jpg"
-        },
-        "to": {
-            "name": "Jenny Forster2",
-            "avatarFileName": "jenny-forster.jpg"
-        },
-        "title": "This is the title of an email"},
-    {
-        "type": "meeting",
-        "date": "25/12/2012",
-        "time": "11.34",
-        "createdBy": {
-            "name": "Jenny Forster3",
-            "avatarFileName": "jenny-forster.jpg"
-        },
-        "title": "This is the title of a meeting"},
-    {
-        "type": "note",
-        "date": "25/12/2012",
-        "time": "11.34",
-        "createdBy": {
-            "name": "Jenny Forster4",
-            "avatarFileName": "jenny-forster.jpg"
-        },
-        "title": "This is the title of a note"}
-    ];
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.directive('ngIf', function() {
+//     return {
+//         link: function(scope, element, attrs) {
+//             if(scope.$eval(attrs.ngIf)) {
+//                 // remove '<div ng-if...></div>'
+//                 element.replaceWith(element.children());
+//             } else {
+//                 element.replaceWith(' ');
+//             }
+//         }
+//     };
+// });
+//
+//
+// app.controller("MyCtrl", function($scope) {
+//     $scope.data = {};
+//     $scope.showFrom = function(message) {
+//         return message.hasOwnProperty('from');
+//     };
+//     $scope.showCreatedBy= function(message) {
+//         return message.hasOwnProperty('createdBy');
+//     };
+//     $scope.showTo= function(message) {
+//         return message.hasOwnProperty('to');
+//     };
+//     $scope.data.messages = [
+//         {
+//         "type": "phone",
+//         "date": "25/12/2012",
+//         "time": "11.34",
+//         "createdBy": {
+//             "name": "Jenny Forster1",
+//             "avatarFileName": "jenny-forster.jpg"
+//         },
+//         "from": {
+//             "name": "Jenny Forster1",
+//             "avatarFileName": "jenny-forster.jpg"
+//         },
+//         "to": {
+//             "name": "Daniel Craig1",
+//             "avatarFileName": "daniel-craig.jpg"
+//         },
+//         "title": "This is the title of a phone call"},
+//     {
+//         "type": "email",
+//         "date": "25/12/2012",
+//         "time": "11.34",
+//         "createdBy": {
+//             "name": "Jenny Forster2",
+//             "avatarFileName": "jenny-forster.jpg"
+//         },
+//         "from": {
+//             "name": "Daniel Craig2",
+//             "avatarFileName": "daniel-craig.jpg"
+//         },
+//         "to": {
+//             "name": "Jenny Forster2",
+//             "avatarFileName": "jenny-forster.jpg"
+//         },
+//         "title": "This is the title of an email"},
+//     {
+//         "type": "meeting",
+//         "date": "25/12/2012",
+//         "time": "11.34",
+//         "createdBy": {
+//             "name": "Jenny Forster3",
+//             "avatarFileName": "jenny-forster.jpg"
+//         },
+//         "title": "This is the title of a meeting"},
+//     {
+//         "type": "note",
+//         "date": "25/12/2012",
+//         "time": "11.34",
+//         "createdBy": {
+//             "name": "Jenny Forster4",
+//             "avatarFileName": "jenny-forster.jpg"
+//         },
+//         "title": "This is the title of a note"}
+//     ];
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -124,7 +262,126 @@ app.controller("MyCtrl", function($scope) {
 
 
 
-// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// ////////////
+// // ROUTES //
+// ////////////
+//
+// app.config(function($routeProvider, $locationProvider){
+//
+//   $routeProvider
+//     .when('/', {
+//       templateUrl: 'templates/wines-index.html',
+//       controller: 'WinesIndexCtrl'
+//     })
+//     .when('/wines/:id', {
+//       // template: '{{wine.name}}',
+//       templateUrl: 'templates/wines-show.html',
+//       controller: 'WinesShowCtrl'
+//     })
+//     .otherwise({
+//       redirectTo: '/'
+//     });
+//
+//   $locationProvider.html5Mode({
+//     enabled: true,
+//     requireBase: false
+//   });
+//
+// })
+//
+// /////////////////
+// // CONTROLLERS //
+// /////////////////
+//
+// app.controller('WinesIndexCtrl',function($scope, WineService){
+//   console.log("Wine Index")
+//   $scope.wines = WineService.query();
+// })
+//
+// app.controller('WinesShowCtrl',function($scope, WineService, $routeParams){
+//   console.log("Wine Show")
+//   $scope.wine = WineService.get($routeParams.id);
+// })
+//
+// ////////////
+// // MODELS //
+// ////////////
+//
+// app.factory('WineService', function(){
+//
+//   var WineService = {};
+//
+//   WineService.query = function(){
+//     return ALL_WINES;
+//   }
+//
+//   WineService.get = function(id){
+//     var id = parseInt(id);
+//     return ALL_WINES.find(function(wine){
+//       return wine.id == id;
+//     });
+//   }
+//
+//   return WineService;
+//
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
 //
 //
 //
